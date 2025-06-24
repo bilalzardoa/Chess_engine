36790cfaee3b4402700800924702fb6998527ce8
bilalzardoa <94915088+bilalzardoa@users.noreply.github.com>
2025-06-24 20:20:06 +0200

    make move function and fast API integration

--- src/game.py
+++ src/game.py
@@ -1,7 +1,25 @@
-from board import init_board
+import chess
+from fastapi import FastAPI
 
-# new board gets initialized at the start of the game
-board_str , board_int = init_board()
+app = FastAPI()
 
+# Init board (correct met hoofdletter B)
+board = chess.Board()
+
+@app.get("/")
+def hello_world():
+    return {"message":"hello world!"}
+
+@app.post("/move")
+def make_move(move: str):
+    try:
+        move_obj = chess.Move.from_uci(move)
+        if move_obj in board.legal_moves:
+            board.push(move_obj)
+            return {"status": "ok", "fen": board.fen()}
+        else:
+            return {"status": "illegal"}
+    except ValueError:
+        return {"status": "invalid_format"}
 
 
